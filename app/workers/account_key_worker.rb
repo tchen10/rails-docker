b6441da57da3363bafca9137ca1141fbdcc5417e
Tiffany Chen <tchen@thoughtworks.com>
2017-10-24 21:57:12 -0400

    Spike for sneakers worker.

--- app/workers/account_key_worker.rb
+++ app/workers/account_key_worker.rb
@@ -1,18 +1,19 @@
 class AccountKeyWorker
-  include Sidekiq::Worker
+  include Sneakers::Worker
+  from_queue 'account_keys'
 
-  def perform(email, key)
+  def work(msg)
+    Rails.logger.info "#{self.class.name}: Received #{msg}"
+    message = JSON.parse(msg)
+    raise MissingAttributeError if message['email'].nil? || message['account_key'].nil?
+
+    response_email = message['email']
+    response_account_key = message['account_key']
+
     begin
-    Rails.logger.info "#{self.class.name}: Retrieving account key for #{email}"
-    AccountKeyGateway.new.account_key_for email, key
-
-    rescue GatewayError => gateway
-      Rails.logger.info "#{self.class.name}: Failed to retrieve account key for #{email}. Retry in 10 minutes: #{gateway.message}"
-      self.class.perform_in(10.minutes, email, key)
-    rescue ActiveRecord::RecordNotFound => not_found
-      Rails.logger.info "#{self.class.name}: Update account key for #{email} failed and will not be re-tryed: #{not_found.message}"
-    rescue StandardError => e
-      Rails.logger.info "#{self.class.name}: Job failed and will not be re-tryed. #{email}: #{e.message}"
+      UserAccountKeyService.update(response_email, response_account_key)
+    rescue ActiveRecord::RecordNotFound => e
+      Rails.logger.info "#{self.class.name}: User associated with email #{response_email} not found. #{e.message}"
     end
   end
 end
