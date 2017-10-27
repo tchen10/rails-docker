12a1efcbb9a4d41f045b8c94e64d825be27ed702
Tiffany Chen <tchen@thoughtworks.com>
2017-10-27 19:11:22 -0400

    Make queues durable.

--- app/services/event_publisher.rb
+++ app/services/event_publisher.rb
@@ -5,10 +5,10 @@ class EventPublisher
   end
 
   def publish
-    queue = channel.queue(@queue_name)
+    queue = channel.queue(@queue_name, :durable => true)
     exchange = channel.default_exchange
-    exchange.publish(@event.to_json, {:routing_key => queue.name,
-                                      :content_type => 'application/json'})
+    exchange.publish(@event.to_json, { :routing_key => queue.name,
+                                       :content_type => 'application/json' })
     Rails.logger.info "#{self.class.name}: #{@event.to_json} published to queue #{@queue_name}"
     @connection.close
   end
--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -3,6 +3,7 @@ require 'sneakers'
 Sneakers.configure(:amqp => ENV['MESSAGE_QUEUE_URL'],
                    :timeout_job_after => 5,
                    :prefetch => 10,
-                   :threads => 10)
+                   :threads => 10,
+                   :durable => true)
 
 Sneakers.logger.level = Logger::INFO
