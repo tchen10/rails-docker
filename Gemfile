93e9a822c4aa804848ef71ed2def029a99b330b4
Tiffany Chen <tchen@thoughtworks.com>
2017-10-29 12:19:43 -0400

    Add spec for event publisher.

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ group :development, :test do
   gem 'shoulda-matchers', '~> 3.1'
   gem 'factory_bot_rails'
   gem 'pry'
+  gem 'bunny-mock'
 end
 
 group :development do
--- app/services/event_publisher.rb
+++ app/services/event_publisher.rb
@@ -5,8 +5,8 @@ class EventPublisher
   end
 
   def publish
-    queue = channel.queue(@queue_name, :durable => true)
-    exchange = channel.default_exchange
+    exchange = channel.direct 'users-api'
+    queue = channel.queue(@queue_name, :durable => true).bind(exchange, :routing_key => @queue_name)
     exchange.publish(@event.to_json, { :routing_key => queue.name,
                                        :content_type => 'application/json' })
     Rails.logger.info "#{self.class.name}: #{@event.to_json} published to queue #{@queue_name}"
@@ -23,10 +23,6 @@ class EventPublisher
     begin
       @connection = Bunny.new(ENV['MESSAGE_QUEUE_URL'])
       @connection.start
-    rescue Bunny::TCPConnectionFailed => e
-      Rails.logger.warn "Connection to #{ENV['MESSAGE_QUEUE_URL']} failed"
-    rescue Bunny::PossibleAuthenticationFailureError => e
-      Rails.logger.warn "Could not authenticate as #{@connection.username}"
     end
   end
 end
